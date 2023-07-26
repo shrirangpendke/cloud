da47df18e747cbfec79b0ad4b38252936b12cba5
Shrirang Pendke <shrirangpendkes@gmail.com>
2023-07-26 15:33:33 +0530

    new srv

---
+++ flask/flsk.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+
+def root():
+    return "welcome to my flask application V-1.0"
+
+app.run("0.0.0.0", port=4000, debug=True)
